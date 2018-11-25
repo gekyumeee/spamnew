9dda3ab0c6e6ac50a06b85a63414fbffbb6301e1
gekyumeee <45327532+gekyumeee@users.noreply.github.com>
2018-11-25 21:02:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam2') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`اراب جيمنج فوق`)
+        message.channel.send(`[ " ${x} " ]اراب جيمنج فوق`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam2') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`اراب جيمنج فوق`)
+        message.channel.send(`[ " ${x} " ]اراب جيمنج فوق`)
           .then(m => {
             count++;
           })
